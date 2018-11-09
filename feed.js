9ebea3a9b4bedfa536614c17621d44b653ba2bc8
Lucifer <ramanandsirvi21@gmail.com>
2018-11-09 11:30:56 +0530

    Final build

--- feed.js
+++ feed.js
@@ -1,20 +1,20 @@
 const ex = require("xlsx");
-const admin = require('firebase-admin');
-const functions = require('firebase-functions');
+const admin = require("firebase-admin");
+const functions = require("firebase-functions");
 
-var serviceAccount = require('C:\\Users\\dbita\\Desktop\\node-alumni\\alumni-db-94ff2-firebase-adminsdk-pzotv-78f8020458.json');
+var serviceAccount = require("C:\\Users\\dbita\\Desktop\\node-alumni\\alumni-db-94ff2-firebase-adminsdk-pzotv-78f8020458.json");
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount)
 });
 
 var db = admin.firestore();
-var input = ex.readFile('data5.xlsx');
+var input = ex.readFile("data5.xlsx");
 
 console.log(input.Sheets.Sheet1);
 var sheets = input.Sheets.Sheet1;
 var sheets_json = ex.utils.sheet_to_json(sheets);
-var docRef = db.collection('alumni').doc('students');
+var docRef = db.collection("alumni").doc("students");
 
 // sheets_json.forEach(sheet => {
 //     var addStudent = docRef.collection(sheet.department).doc(sheet.facultyid).set({
@@ -31,19 +31,24 @@ var docRef = db.collection('alumni').doc('students');
 // });
 
 sheets_json.forEach(sheet => {
+  console.log("-----------------------------------");
+  console.log(sheet);
+  dob_array = sheet.dob.split("/");
 
-    console.log("-----------------------------------");
-    console.log(sheet);
-
-    var addStudent = docRef.collection('CVE').doc(sheet.usn).set({
-        bdate: sheet.dob,
-        contact: sheet.phone,
-        dob: sheet.dob,
-        email: sheet.email,
-        name: sheet.name
-    }).then(usn => {
-        console.log('Added student: ', sheet.usn);
-    }).catch(err => {
-        console.log(err);
+  var addStudent = docRef
+    .collection("CVE")
+    .doc(sheet.usn)
+    .set({
+      bdate: dob_array[0] + "-" + dob_array[1],
+      contact: sheet.phone,
+      dob: sheet.dob,
+      email: sheet.email,
+      name: sheet.name
+    })
+    .then(usn => {
+      console.log("Added student: ", sheet.usn);
+    })
+    .catch(err => {
+      console.log(err);
     });
 });
